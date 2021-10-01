a72971db7b33e5174d06b53c8297619a62db3375
corvinus-if6uta <kende.szekerka@stud.uni-corvinus.hu>
2021-10-01 21:41:45 +0200

    asd

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listUsers = new System.Windows.Forms.ListBox();
+            this.lblFullName = new System.Windows.Forms.Label();
+            this.txtFullName = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnAddToFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listUsers
+            // 
+            this.listUsers.FormattingEnabled = true;
+            this.listUsers.ItemHeight = 16;
+            this.listUsers.Location = new System.Drawing.Point(34, 33);
+            this.listUsers.Name = "listUsers";
+            this.listUsers.Size = new System.Drawing.Size(233, 388);
+            this.listUsers.TabIndex = 0;
+            // 
+            // lblFullName
+            // 
+            this.lblFullName.AutoSize = true;
+            this.lblFullName.Location = new System.Drawing.Point(299, 36);
+            this.lblFullName.Name = "lblFullName";
+            this.lblFullName.Size = new System.Drawing.Size(46, 17);
+            this.lblFullName.TabIndex = 1;
+            this.lblFullName.Text = "label1";
+            // 
+            // txtFullName
+            // 
+            this.txtFullName.Location = new System.Drawing.Point(355, 33);
+            this.txtFullName.Name = "txtFullName";
+            this.txtFullName.Size = new System.Drawing.Size(169, 22);
+            this.txtFullName.TabIndex = 3;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(355, 80);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(168, 23);
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.Text = "button1";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnAddToFile
+            // 
+            this.btnAddToFile.Location = new System.Drawing.Point(355, 127);
+            this.btnAddToFile.Name = "btnAddToFile";
+            this.btnAddToFile.Size = new System.Drawing.Size(168, 23);
+            this.btnAddToFile.TabIndex = 6;
+            this.btnAddToFile.Text = "button2";
+            this.btnAddToFile.UseVisualStyleBackColor = true;
+            this.btnAddToFile.Click += new System.EventHandler(this.btnAddToFile_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(577, 450);
+            this.Controls.Add(this.btnAddToFile);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.txtFullName);
+            this.Controls.Add(this.lblFullName);
+            this.Controls.Add(this.listUsers);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox listUsers;
+        private System.Windows.Forms.Label lblFullName;
+        private System.Windows.Forms.TextBox txtFullName;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnAddToFile;
     }
 }
 
--- UserMaintenance/Form1.cs
+++ UserMaintenance/Form1.cs
@@ -7,14 +7,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using UserMaintenance.Entities;
 
 namespace UserMaintenance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
+            lblFullName.Text = Resource1.FullName; // label1
+            btnAdd.Text = Resource1.Add; // button1
+            btnAddToFile.Text = Resource1.AddToFile; // button2
+
+            // listbox1
+            listUsers.DataSource = users;
+            listUsers.ValueMember = "ID";
+            listUsers.DisplayMember = "FullName";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var u = new User()
+            {
+                FullName = txtFullName.Text
+             };
+        users.Add(u);
+        }
+
+        private void btnAddToFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveFileDialog1 = new SaveFileDialog();
+            SaveFileDialog1.ShowDialog();
+
         }
     }
 }
