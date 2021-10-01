509dba4b1f251fb40ed264752baf0684df397583
corvinus-if6uta <kende.szekerka@stud.uni-corvinus.hu>
2021-10-01 21:44:15 +0200

    delete

--- UserMaintenance/Form1.Designer.cs
+++ UserMaintenance/Form1.Designer.cs
@@ -33,6 +33,7 @@
             this.txtFullName = new System.Windows.Forms.TextBox();
             this.btnAdd = new System.Windows.Forms.Button();
             this.btnAddToFile = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // listUsers
@@ -80,11 +81,21 @@
             this.btnAddToFile.UseVisualStyleBackColor = true;
             this.btnAddToFile.Click += new System.EventHandler(this.btnAddToFile_Click);
             // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(356, 169);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(166, 32);
+            this.btnDelete.TabIndex = 7;
+            this.btnDelete.Text = "button1";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(577, 450);
+            this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.btnAddToFile);
             this.Controls.Add(this.btnAdd);
             this.Controls.Add(this.txtFullName);
@@ -104,6 +115,7 @@
         private System.Windows.Forms.TextBox txtFullName;
         private System.Windows.Forms.Button btnAdd;
         private System.Windows.Forms.Button btnAddToFile;
+        private System.Windows.Forms.Button btnDelete;
     }
 }
 
