f5d5e833000bc0c5eb4371ad4e18bd2cb6bf1b39
corvinus-if6uta <kende.szekerka@stud.uni-corvinus.hu>
2021-10-29 23:17:39 +0200

    webszolgaltatas hivasa

--- gyak6/Form1.cs
+++ gyak6/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using gyak6.MnbServiceReference;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +17,31 @@ namespace gyak6
         {
             InitializeComponent();
         }
+
+        public void GetExchangeRates()
+        {
+            return;
+        }
+
+        public void asd()
+        {
+            var mnbService = new MNBArfolyamServiceSoapClient();
+
+            var request = new GetExchangeRatesRequestBody()
+            {
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-30"
+            };
+
+            var response = MnbService.GetExchangeRates(request);
+
+            var result = response.GetExchangeRatesResult;
+        }
+
+        public object MnbService { get => mnbService; set => mnbService = value; 
+        
+    }
 
         private void Form1_Load(object sender, EventArgs e)
         {
---
+++ gyak6/GetExchangeRatesRequestBody.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak6
+{
+    class GetExchangeRatesRequestBody
+    {
+        public string currencyNames { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+}
