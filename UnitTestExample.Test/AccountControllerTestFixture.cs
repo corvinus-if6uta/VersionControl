7d39ca08c7c625c4bf89d99cdd7d522cdf84a7f3
corvinus-if6uta <kende.szekerka@stud.uni-corvinus.hu>
2021-11-26 22:51:55 +0100

    Register függvény HappyPath

--- UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -48,5 +48,24 @@ namespace UnitTestExample.Test
             // Assert
             Assert.AreEqual(expectedResult, actualResult);
         }
+
+        [
+    Test,
+    TestCase("kende@gmail.com", "Jelszo12345"),
+    TestCase("kendeee@freemail.hu", "jELSZO6543"),
+]
+        public void TestRegisterHappyPath(string email, string password)
+        {
+            // Arrange
+            var accountController = new AccountController();
+
+            // Act
+            var actualResult = accountController.Register(email, password);
+
+            // Assert
+            Assert.AreEqual(email, actualResult.Email);
+            Assert.AreEqual(password, actualResult.Password);
+            Assert.AreNotEqual(Guid.Empty, actualResult.ID);
+        }
     }
 }
