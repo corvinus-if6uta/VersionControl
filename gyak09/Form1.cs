639eb4026202450b38bc0e5e483bfd4634b8cf0e
corvinus-if6uta <kende.szekerka@stud.uni-corvinus.hu>
2021-12-03 22:37:16 +0100

    Szimuláció

--- gyak09/Form1.cs
+++ gyak09/Form1.cs
@@ -1,13 +1,10 @@
 ﻿using gyak09.Entities;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace gyak09
@@ -27,6 +24,24 @@ namespace gyak09
             Population = GetPopulation(@"C:\Temp\nép.csv");
             BirthProbabilities = GetBirthProbabilities(@"C:\Temp\születés.csv");
             DeathProbabilities = GetDeathProbabilities(@"C:\Temp\halál.csv");
+
+            for (int year = 2005; year <= 2024; year++)
+            {
+                // Végigmegyünk az összes személyen
+                for (int i = 0; i < Population.Count; i++)
+                {
+                    // Ide jön a szimulációs lépés
+                }
+
+                int nbrOfMales = (from x in Population
+                                  where x.Gender == Gender.Male && x.IsAlive
+                                  select x).Count();
+                int nbrOfFemales = (from x in Population
+                                    where x.Gender == Gender.Female && x.IsAlive
+                                    select x).Count();
+                Console.WriteLine(
+                    string.Format("Év:{0} Fiúk:{1} Lányok:{2}", year, nbrOfMales, nbrOfFemales));
+            }
         }
 
         public List<Person> GetPopulation(string csvpath)
@@ -50,7 +65,7 @@ namespace gyak09
             return population;
         }
 
-        public List<BirthProbability>GetBirthProbabilities(string csvpath)
+        public List<BirthProbability> GetBirthProbabilities(string csvpath)
         {
             List<BirthProbability> birth = new List<BirthProbability>();
 
@@ -61,7 +76,7 @@ namespace gyak09
                     var line = sr.ReadLine().Split(';');
                     birth.Add(new BirthProbability()
                     {
-                        Age = int.Parse(line[0]),                       
+                        Age = int.Parse(line[0]),
                         NbrOfChildren = int.Parse(line[1]),
                         P = double.Parse(line[2])
                     });
