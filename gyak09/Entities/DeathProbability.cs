c50279f3c4e3593fe26fc843e500e99f0569bf10
corvinus-if6uta <kende.szekerka@stud.uni-corvinus.hu>
2021-12-03 22:48:27 +0100

    Szimulációs lépés

--- gyak09/Entities/DeathProbability.cs
+++ gyak09/Entities/DeathProbability.cs
@@ -8,8 +8,8 @@ namespace gyak09.Entities
 {
     public class DeathProbability
     {
+        public Gender Gender { get; set; }
         public int Age { get; set; }
-        public int NbrOfChildren { get; set; }
         public double P { get; set; }
     }
 }
