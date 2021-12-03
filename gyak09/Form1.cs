b08f4cd7d8001a4c150aba2e79ea59e8d1aa2974
corvinus-if6uta <kende.szekerka@stud.uni-corvinus.hu>
2021-12-03 22:31:46 +0100

    Véletlenszám generálás

--- gyak09/Form1.cs
+++ gyak09/Form1.cs
@@ -18,6 +18,7 @@ namespace gyak09
         List<Person> Population = new List<Person>();
         List<BirthProbability> BirthProbabilities = new List<BirthProbability>();
         List<DeathProbability> DeathProbabilities = new List<DeathProbability>();
+        Random rng = new Random(1234);
 
         public Form1()
         {
