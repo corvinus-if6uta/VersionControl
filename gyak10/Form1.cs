b9f7cd9ae842fc1f177e7e8320f23c75c9ec02e0
corvinus-if6uta <kende.szekerka@stud.uni-corvinus.hu>
2021-12-10 15:51:37 +0100

    Iterációs lépés felépítése

--- gyak10/Form1.cs
+++ gyak10/Form1.cs
@@ -21,6 +21,8 @@ namespace gyak10
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+
+
         public Form1()
         {
             InitializeComponent();
@@ -28,15 +30,46 @@ namespace gyak10
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
-
+            gc.GameOver += Gc_GameOver;
 
             for (int i = 0; i < populationSize; i++)
             {
                 gc.AddPlayer(nbrOfSteps);
             }
+            gc.Start();
 
 
-             gc.Start();
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            var playerList = from p in gc.GetCurrentPlayers()
+                             orderby p.GetFitness() descending
+                             select p;
+            var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            generation++;
+            label1.Text = string.Format(
+                "{0}. generáció",
+                generation);
+
+            gc.ResetCurrentLevel();
+            foreach (var p in topPerformers)
+            {
+                var b = p.Brain.Clone();
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b);
+
+                if (generation % 3 == 0)
+                    gc.AddPlayer(b.Mutate().ExpandBrain(nbrOfStepsIncrement));
+                else
+                    gc.AddPlayer(b.Mutate());
+            }
+            gc.Start();
+
+
         }
     }
 }
